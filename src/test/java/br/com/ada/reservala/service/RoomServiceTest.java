8821ad434d5638c767f5301c933ef97de1988999
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-06-04 22:54:43 -0300

    Feat: inclusão dos primeiros testes da funcionalidade update para RoomService e RoomRepository

--- src/test/java/br/com/ada/reservala/service/RoomServiceTest.java
+++ src/test/java/br/com/ada/reservala/service/RoomServiceTest.java
@@ -4,20 +4,26 @@ import br.com.ada.reservala.domain.Room;
 import br.com.ada.reservala.repository.RoomRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class RoomServiceTest {
-    private RoomService roomService;
+
+    @Mock
     private RoomRepository roomRepository;
 
-    @BeforeEach
-    public void setUp() {
-        roomRepository = mock(RoomRepository.class);
-        roomService = new RoomService(roomRepository);
-    }
+    @InjectMocks
+    private RoomService roomService;
 
     @Test
     void createRoom() {
@@ -42,7 +48,26 @@ class RoomServiceTest {
     }
 
     @Test
-    void updateRoom() {
+    void testUpdateRoom_Success() {
+        Room actualRoom = new Room(2, "Standard", 350, true);
+        Room updatedRoom = new Room(2, "Deluxe", 400, false);
+        Mockito.when(roomRepository.updateRoom(eq(updatedRoom)))
+                .thenReturn(updatedRoom);
+
+        Room result = roomService.updateRoom(updatedRoom);
+
+        assertNotNull(result);
+        assertEquals(updatedRoom, result);
+        Mockito.verify(roomRepository, times(1)).updateRoom(eq(updatedRoom));
+    }
+
+    @Test
+    void testUpdateRoom_InvalidValues() {
+        Room invalidRoom = new Room(2, "Standard", -350, true);
+        assertThrows(IllegalArgumentException.class, () -> {
+            roomService.updateRoom(invalidRoom);
+        });
+        verify(roomRepository, never()).updateRoom(any(Room.class));
     }
 
     @Test
